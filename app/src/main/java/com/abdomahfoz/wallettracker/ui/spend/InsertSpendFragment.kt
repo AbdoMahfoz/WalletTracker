8165d44ea4a0d3b937a34cdb37d2217e2fe4809e
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2020-05-11 09:03:08 +0200

    Maps UI

--- app/src/main/java/com/abdomahfoz/wallettracker/ui/spend/InsertSpendFragment.kt
+++ app/src/main/java/com/abdomahfoz/wallettracker/ui/spend/InsertSpendFragment.kt
@@ -1,11 +1,20 @@
 package com.abdomahfoz.wallettracker.ui.spend
 
+import android.Manifest
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -19,14 +28,16 @@ import com.abdomahfoz.wallettracker.viewModels.ViewModelFactory
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import java.util.*
-
 
 class InsertSpendFragment : Fragment() {
     private val spendDate = Calendar.getInstance()
     private lateinit var binding : FragmentInsertSpendBinding
     private lateinit var viewModel: SpendViewModel
+    private var mapFrag: SupportMapFragment? = null
+    private var markerPosition: LatLng = LatLng(0.0, 0.0)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(
@@ -39,15 +50,83 @@ class InsertSpendFragment : Fragment() {
         binding.date = spendDate.time
         handleDate()
         handleSubmit()
-        val mapFrag = childFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
+        mapFrag = childFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
+        initMaps()
+        return binding.root
+    }
+    private fun initMaps() {
         mapFrag?.getMapAsync{
-            it.apply {
-                val sydney = LatLng(-33.852, 151.211)
-                addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-                moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            it.isMyLocationEnabled = true
+            it.isBuildingsEnabled = false
+            it.isIndoorEnabled = false
+            it.isTrafficEnabled = false
+            it.uiSettings.isMyLocationButtonEnabled = true
+            val fineAccess = ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+            val coarseAccess = ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+            if (!fineAccess && !coarseAccess) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                    5000
+                )
+            } else {
+                val lm : LocationManager? = getSystemService(requireContext(), LocationManager::class.java)
+                var currentMarker: Marker? = null
+                it.apply {
+                    val lastLocation = lm?.getLastKnownLocation(lm.getBestProvider(Criteria(), false)!!)
+                    if(lastLocation != null) {
+                        val latLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+                        markerPosition = latLng
+                        currentMarker = addMarker(MarkerOptions().position(latLng).title("Your Location"))
+                        moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                    }
+                    setOnCameraMoveListener {
+                        markerPosition = it.cameraPosition.target
+                        if(currentMarker == null) {
+                            currentMarker = addMarker(
+                                MarkerOptions().position(it.cameraPosition.target).title("Your Location")
+                            )
+                        } else {
+                            currentMarker?.position = it.cameraPosition.target
+                        }
+                    }
+                    setOnMapClickListener { clickLocation ->
+                        markerPosition = clickLocation
+                        currentMarker?.remove()
+                        currentMarker = addMarker(
+                            MarkerOptions().position(clickLocation).title("Your Location")
+                        )
+                        moveCamera(CameraUpdateFactory.newLatLng(clickLocation))
+                    }
+                }
+                lm?.requestSingleUpdate(Criteria(), object : LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        if(location != null) {
+                            val marker = LatLng(location.latitude, location.longitude)
+                            markerPosition = marker
+                            it.apply {
+                                currentMarker = addMarker(MarkerOptions().position(marker).title("Your Location"))
+                                moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 15f))
+                            }
+                        }
+                    }
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+                    override fun onProviderEnabled(provider: String?) {}
+                    override fun onProviderDisabled(provider: String?) {}
+                }, Looper.myLooper())
             }
         }
-        return binding.root
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == 5000) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED ||
+               grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                initMaps()
+            }
+        }
     }
     private fun handleDate() {
         binding.changeDateButton.setOnClickListener{
@@ -79,7 +158,6 @@ class InsertSpendFragment : Fragment() {
                 )
             )
             findNavController().popBackStack()
-            //requireNotNull(activity).onBackPressed()
         }
     }
 }
